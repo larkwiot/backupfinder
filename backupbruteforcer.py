047a0f19c7dd04b989db30893e144e7e7f265732
larkwiot <22042193+larkwiot@users.noreply.github.com>
2017-09-30 18:23:57 -0400

    Script spits what it finds instantly

--- backupbruteforcer.py
+++ backupbruteforcer.py
@@ -1,5 +1,5 @@
 import requests, time
-from multiprocessing import Process, Manager
+from multiprocessing import Process
 
 def findchar(enteredString, searchString, occurence):
     ecx = 1
@@ -46,7 +46,13 @@ def obtain():
         except requests.exceptions.RequestException:
             return False
 
-def attempt(http, link, fyle, fiex, iam, urls):
+def test(procName, request, code):
+    if code != 404:
+        print procName + " !!! Found something!"
+        print "[#] URL ::: " + request
+        print "|__ COD ::: " + str(code)
+
+def attempt(http, link, fyle, fiex, iam):
     things = ['conf', 'bak', 'swp', 'txt', 'old', 'tar', 'tar.gz', 'tar.bz2', 'zip', 'inc',
               'asa', 'tgz', 'gz', 'rar', 'java', 'py', 'js', 'pdf', 'doc', 'docx', 'rtf',
               'pl', 'o', 'obj', 'jar', 'inf', 'exe', 'c', 'bat', 'asm', 'awk', 's', 'sh',
@@ -57,24 +63,21 @@ def attempt(http, link, fyle, fiex, iam, urls):
     for ext in things:
         req = http + link + fyle + "." + fiex + "." + ext
         connection = requests.head(req)
-        urls.append(req)
-        urls.append(connection.status_code)
+        test(iam, req, connection.status_code)
         time.sleep(0.001)
     
     print iam + ' ::: Attempt method [2/4]'
     for repl in things:
         req = http + link + fyle + "." + repl
         connection = requests.head(req)
-        urls.append(req)
-        urls.append(connection.status_code)
+        test(iam, req, connection.status_code)
         time.sleep(0.001)
 
     print iam + ' ::: Attempt method [3/4]'
     for pre in things:
         req = http + link + pre + fyle + "." + fiex
         connection = requests.head(req)
-        urls.append(req)
-        urls.append(connection.status_code)
+        test(iam, req, connection.status_code)
         time.sleep(0.001)
 
     print iam + ' ::: Attempt method [4/4]'
@@ -82,8 +85,7 @@ def attempt(http, link, fyle, fiex, iam, urls):
         for main in things:
             req = http + link + main + fyle + fiex + "." + things[exten]
             connection = requests.head(req)
-            urls.append(req)
-            urls.append(connection.status_code)
+            test(iam, req, connection.status_code)
             time.sleep(0.001)
     print iam + ' ::: Done.'
 
@@ -96,30 +98,16 @@ if __name__ == '__main__':
             atmp -= 1
             continue
 
-    manager = Manager()
-    urls = manager.list()
-
     ecx = 0
     jobs = []
     for argz in conns:
         procName = 'Process -> #' + str(ecx)
-        p = Process(target=attempt, args=(argz[0], argz[1], argz[2], argz[3], procName, urls))
+        p = Process(target=attempt, args=(argz[0], argz[1], argz[2], argz[3], procName))
         jobs.append(p)
         p.start()
         ecx += 1
     time.sleep(5)
     for close in jobs:
         close.join()
-    print "=============\n[i] Results."
-    c200s = 0
-    for search in range(0, len(urls)):
-        if not isinstance(urls[search], basestring) and urls[search] != 404:
-            c200s += 1
-            print "============="
-            print "[" + str(c200s) + "] URL ::: " + urls[search-1].replace("\n", "").replace("\r", "")
-            print "|__ COD ::: " + str(urls[search])
-    print "============="
-    print "[i] " + str(c200s) + " URLs found."
-    print "[i] " + str(len(urls)/2) + " URLs attempted."
-    print "=============\n[*] Done.\n=============\n"
+    print "\n=============\n[*] Done.\n============="
     exit(0)
